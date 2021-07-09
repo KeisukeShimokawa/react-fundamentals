06d029fbae2bbe2dc5649f52ea3ba86f73469b17
KeisukeShimokawa <shimoke4869@gmail.com>
2021-07-09 14:50:43 +0000

    JSX内で配列の操作を行う方法を追加

--- src/exercise/08.tsx
+++ src/exercise/08.tsx
@@ -1,39 +1,49 @@
 // Rendering Lists
 // http://localhost:3000/isolated/exercise/08.tsx
 
-import * as React from 'react'
+import * as React from 'react';
 
-type Item = {id: string; value: string}
+type Item = {id: string; value: string};
 
 const allItems: Array<Item> = [
   {id: 'apple', value: '🍎 apple'},
   {id: 'orange', value: '🍊 orange'},
   {id: 'grape', value: '🍇 grape'},
   {id: 'pear', value: '🍐 pear'},
-]
+];
 
 function typedBoolean<T>(
   value: T,
 ): value is Exclude<T, false | null | undefined | '' | 0> {
-  return Boolean(value)
+  return Boolean(value);
 }
 
 function App() {
-  const [items, setItems] = React.useState(allItems)
+  const [items, setItems] = React.useState(allItems);
 
   function addItem() {
-    const itemIds = items.map(i => i.id)
+    const itemIds = items.map(i => i.id);
     const newItems = [
       ...items,
       allItems.find(i => !itemIds.includes(i.id)),
-    ].filter(typedBoolean)
-    setItems(newItems)
+    ].filter(typedBoolean);
+    setItems(newItems);
   }
 
   function removeItem(item: Item) {
-    const newItems = items.filter(i => i.id !== item.id)
-    setItems(newItems)
+    const newItems = items.filter(i => i.id !== item.id);
+    setItems(newItems);
   }
+
+  console.log(
+    items.map(item => (
+      <li>
+        <button onClick={() => removeItem(item)}>remove</button>{' '}
+        <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
+        <input id={`${item.id}-input`} defaultValue={item.value} />
+      </li>
+    )),
+  );
 
   return (
     <div className="keys">
@@ -49,12 +59,34 @@ function App() {
             - <input> with an id that matches the htmlFor and a defaultValue set to item.value
           💰 You can set the `id` and `htmlFor` to `${item.id}-input` to make it globally unique
          */}
+
+        {/* map((item, index) => (
+           <li key={index}> とかの表現でもいい
+         )) */}
+        {items.map(item => (
+          <li key={item.id}>
+            <button onClick={() => removeItem(item)}>remove</button>{' '}
+            <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
+            <input
+              id={`${item.id}-input`}
+              type="text"
+              defaultValue={item.value}
+            />
+          </li>
+        ))}
+
+        {/* 
+            React要素は、要素を追跡するための key プロップが必要
+            アイテムを再描画する際に、Reactが対象のアイテムがリストの
+            中のどの位置に挿入・削除されたのかを知らなければ、前後を
+            比較することが難しくなる
+        */}
       </ul>
     </div>
-  )
+  );
 }
 
-export {App}
+export {App};
 
 /*
 eslint
