9b2f641a577f2cfe5e653944bcc2acce602b5385
KeisukeShimokawa <shimoke4869@gmail.com>
2021-07-09 13:47:22 +0000

    変数と関数コンポーネントのPropsに対する型の指定方法を追加

--- src/exercise/05.tsx
+++ src/exercise/05.tsx
@@ -3,24 +3,29 @@
 
 // 🦺 add type definitions for each function
 const operations = {
-  '+': (left, right) => left + right,
-  '-': (left, right) => left - right,
-  '*': (left, right) => left * right,
-  '/': (left, right) => left / right,
-}
+  '+': (left: number, right: number): number => left + right,
+  '-': (left: number, right: number): number => left - right,
+  '*': (left: number, right: number): number => left * right,
+  '/': (left: number, right: number): number => left / right,
+};
 
 // 🦺 create a type called CalculatorProps
+type CalculatorProps = {
+  left: number;
+  operator: '+' | '-' | '*' | '/';
+  right: number;
+};
 
 // 🦺 set the type for this props argument to CalculatorProps
-function Calculator({left, operator, right}) {
-  const result = operations[operator](left, right)
+function Calculator({left, operator, right}: CalculatorProps) {
+  const result = operations[operator](left, right);
   return (
     <div>
       <code>
         {left} {operator} {right} = <output>{result}</output>
       </code>
     </div>
-  )
+  );
 }
 
 function App() {
@@ -32,7 +37,7 @@ function App() {
       <Calculator left={1} operator="*" right={2} />
       <Calculator left={1} operator="/" right={2} />
     </div>
-  )
+  );
 }
 
-export {App}
+export {App};
